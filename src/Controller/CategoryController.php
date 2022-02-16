b38c7f8591a436734c2f3ce900d7e95d9dd9cc3b
mohabadiane <mohamed.badiane@sunu-gal.sn>
2022-02-16 15:06:01 +0100

    Initiations aux forms

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,30 +2,64 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Form\CategoryType;
 use App\Repository\CategoryRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class CategoryController extends AbstractController
 {
     /**
      * @Route("/admin/category/create", name="category_create")
      */
-    public function create(): Response
+    public function create(Request $request, SluggerInterface $sluggerInterface, ManagerRegistry $managerRegistry): Response
     {
-        return $this->render('category/create.html.twig', []);
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $category->setSlug(strtolower($sluggerInterface->slug($category->getName())));
+
+            $em = $managerRegistry->getManager();
+            $em->persist($category);
+            $em->flush();
+
+            return $this->redirectToRoute('homepage');
+        }
+
+        $formView = $form->createView();
+        return $this->render('category/create.html.twig', [
+            'formView' => $formView
+        ]);
     }
 
     /**
      * @Route("/admin/category/{id}/edit",name="category_edit")
      */
-    public function edit($id, CategoryRepository $categoryRepository): Response
+    public function edit(Request $request, $id, CategoryRepository $categoryRepository, ManagerRegistry $managerRegistry): Response
     {
         $category = $categoryRepository->find($id);
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            $em = $managerRegistry->getManager();
+            $em->flush();
+
+            return $this->redirectToRoute('homepage');
+        }
+        $formView = $form->createView();
+
         return $this->render(
             'category/edit.html.twig',
             [
+                'formView' => $formView,
                 'category' => $category
             ]
         );
